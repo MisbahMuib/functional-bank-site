ecb6c2a6b4bc5a640bc208bc79441db62a0846d9
MisbahMuib <misbahmuib2016@gmail.com>
2021-08-21 22:19:22 +0600

    updated

---
+++ js/login.js
@@ -0,0 +1,21 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+    //get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+
+    //get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+
+    //input box
+    if (userEmail == 'muib@gmail.com' && userPassword == 'bolajabena') {
+        window.location.href = 'banking.html';
+    }
+    else {
+        console.log('Invalid user');
+    }
+
+
+
+});
+
